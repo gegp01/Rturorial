d955c2a2b4e271c1593c75ddd4ee90a37f6116b6
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2022-03-28 16:23:24 -0600

    Create 1_datos.R

---
+++ 1_datos.R
@@ -0,0 +1,11 @@
+# OBJETIVO. EN ESTE TUTORIAL SE REVISARÁN LAS SIGUIENTES OPERACIONES EN R:
+#
+# 1. Leer y escribir datos de un csv
+# 2. Tipo de datos y distribuciones (integers, proportions, continuous )
+# 2. Hacer un query a la base de datos ()
+# 3. Seccionar datos (grep, sample, )
+# 4. Tabla de frecuencias (chi2)
+# 4. Gráficas
+# 5. Modelo lineal multifacorial, seleccion de modelos AIC.
+# 6. Series de tiempo
+
